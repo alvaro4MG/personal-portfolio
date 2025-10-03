4bcb9af5f5f4d705eddc03f37e6bb0f1be07a2c7
Álvaro <alvaromg8m@gmail.com>
2025-10-03 13:07:18 +0200

    Updated projectsList

--- src/components/home/projectsList.js
+++ src/components/home/projectsList.js
@@ -1,6 +1,22 @@
 export const projectData = [
 	{
 		id: 1,
+		title: 'Unity RPG Demo',
+		description:
+			'Implementation of various mechanics and features to practice and deepen my Unity 2D development skills with a 2D RPG.',
+		link: 'https://github.com/alvaro4MG/curso_8h_RPG',
+		icon: 'fa-brands fa-github'
+	},
+	{
+		id: 2,
+		title: 'Daily C# Leetcode challenge',
+		description:
+			'Daily practice with C# using Leetcode problems.',
+		link: 'https://github.com/alvaro4MG/leetcode-problems',
+		icon: 'fa-brands fa-github'
+	},
+	{
+		id: 3,
 		title: 'Web Portfolio',
 		description:
 			'An online portfolio to show my projects, profile and contact.',
@@ -8,13 +24,13 @@ export const projectData = [
 		icon: 'fa-brands fa-github'
 	},
 	{
-		id: 2,
+		id: 4,
 		title: 'Tech4Diet Website',
 		description:
 			'Redesigned the webpage for the Tech4Diet team of the University of Alicante while colaborating with the research.',
 		link: 'https://tech4d.ua.es',
 		icon: 'fa-solid fa-globe'
-	},
+	},/*
 	{
 		id: 3,
 		title: 'Bachelor\'s Thesis',
@@ -22,15 +38,9 @@ export const projectData = [
 			'Final Year Project (TFG in Spanish) for Computer Engineering, titled "Generation of 3D Human Body Models from 2D Images".',
 		link: 'https://rua.ua.es/dspace/handle/10045/154744',
 		icon: 'fa-solid fa-globe'
-	},
-	{
-		id: 4,
-		title: 'Daily C# Leetcode challenge',
-		description:
-			'Daily practice with C# using Leetcode problems.',
-		link: 'https://github.com/alvaro4MG/leetcode-problems',
-		icon: 'fa-brands fa-github'
-	}
+	},*/
+	
+	
 	/*
 	,{
 		id: ,
