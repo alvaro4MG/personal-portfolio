81ef315e1406b3ec742ac7a8aaac18e5b7ba0e71
Álvaro <alvaromg8m@gmail.com>
2025-10-17 01:51:56 +0200

    Updated CV and Linkedin URL

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                         <i className="fa-brands fa-github"></i>
                     </a>
                     <a
-                        href="https://www.linkedin.com/in/%C3%A1lvaro-mart%C3%ADnez-garc%C3%ADa-739966380/"
+                        href="https://www.linkedin.com/in/alvaro-martinez-garcia-game-dev/"
                         aria-label="LinkedIn"
                         target="_blank"
                         rel="noopener noreferrer"
